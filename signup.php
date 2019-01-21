6d7b69f3dce99fe122c470886d9d9b5e7f99565d
gourishankarn <gourishankarnarayanpur123@gmail.com>
2019-01-21 20:04:54 +0530

    Sign up page and user dashboard done

---
+++ signup.php
@@ -0,0 +1,70 @@
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$conn=mysqli_connect($servername,$username,$password,"selfieless");
+	if(!$conn)
+			{
+					// echo "connection not established";
+				}
+	else
+	{
+					// echo "connection estsblished successfully";
+		$number=$_POST["phone"];
+		
+		$password=$_POST["cpwd"];
+	  if($_POST["pwd"]!=$_POST["cpwd"])
+		{
+			//  echo "password not matching enter properly";
+			 include "signup.html";
+			 ?>
+			 <html>
+			 <body>
+				 <script>
+				 alert("entered passwords not matching");
+				 </script>
+			 </body>
+		 </html>
+		 <?php
+		}
+		else
+		{
+			$query="SELECT * FROM USER";
+			$res=mysqli_query($conn,$query);
+			$flag=0;
+			while($row=mysqli_fetch_assoc($res))
+			{
+				if($row['pho_num']==$number)
+				{
+					$flag=1;
+				}
+			}
+			if($flag==1)
+			{
+				include "signup.html";
+				?>
+				<html>
+				<body>
+					<script>
+					alert("User Already exist, you can login now.");
+					</script>
+				</body>
+			</html>
+			<?php
+			}
+			else
+			{
+					$query="INSERT INTO USER(pho_num,pass) VALUES ($number,'$password')";
+					$res=mysqli_query($conn,$query);
+					if(!$res)
+					{
+						 include "signup.html";
+					}
+					else
+					{
+						 include "user-dashboard.html";
+					}
+			 }
+		 }
+	 }
+?>
